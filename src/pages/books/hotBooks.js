df32c73ae44b4dd79da3bacd191f91c3e06dcc7a
tb-zmy <tb_zmy@163.com>
2019-11-01 14:00:37 +0800

    hotBooks页面展示

--- src/pages/books/hotBooks.js
+++ src/pages/books/hotBooks.js
@@ -1,90 +1,102 @@
-import React, { Component } from 'react'
+import React, {
+    Component
+} from 'react'
 
-import { Table } from 'antd';
+import {
+    Table
+} from 'antd';
 
-const columns = [
-  {
-    title: '姓名',
-    dataIndex: 'name',
-  },
-  {
-    title: '年龄',
-    dataIndex: 'age',
-  },
-  {
-    title: '地址',
-    dataIndex: 'address',
-  },
+const columns = [{
+        title: '姓名',
+        dataIndex: 'name',
+    },
+    {
+        title: '年龄',
+        dataIndex: 'age',
+    },
+    {
+        title: '地址',
+        dataIndex: 'address',
+    },
 ];
 
 const data = [];
 for (let i = 0; i < 46; i++) {
-  data.push({
-    key: i,
-    name: `Edward King ${i}`,
-    age: 32,
-    address: `London, Park Lane no. ${i}`,
-  });
+    data.push({
+        key: i,
+        name: `Edward King ${i}`,
+        age: 32,
+        address: `London, Park Lane no. ${i}`,
+    });
 }
 
 class HotBooks extends Component {
-  state = {
-    selectedRowKeys: [], // Check here to configure the default column
-  };
-
-  onSelectChange = selectedRowKeys => {
-    console.log('selectedRowKeys changed: ', selectedRowKeys);
-    this.setState({ selectedRowKeys });
-  };
-
-  render() {
-    const { selectedRowKeys } = this.state;
-    const rowSelection = {
-      selectedRowKeys,
-      onChange: this.onSelectChange,
-      hideDefaultSelections: true,
-      selections: [
-        {
-          key: 'all-data',
-          text: 'Select All Data',
-          onSelect: () => {
-            this.setState({
-              selectedRowKeys: [...Array(46).keys()], // 0...45
-            });
-          },
-        },
-        {
-          key: 'odd',
-          text: 'Select Odd Row',
-          onSelect: changableRowKeys => {
-            let newSelectedRowKeys = [];
-            newSelectedRowKeys = changableRowKeys.filter((key, index) => {
-              if (index % 2 !== 0) {
-                return false;
-              }
-              return true;
-            });
-            this.setState({ selectedRowKeys: newSelectedRowKeys });
-          },
-        },
-        {
-          key: 'even',
-          text: 'Select Even Row',
-          onSelect: changableRowKeys => {
-            let newSelectedRowKeys = [];
-            newSelectedRowKeys = changableRowKeys.filter((key, index) => {
-              if (index % 2 !== 0) {
-                return true;
-              }
-              return false;
-            });
-            this.setState({ selectedRowKeys: newSelectedRowKeys });
-          },
-        },
-      ],
+    state = {
+        selectedRowKeys: [], // Check here to configure the default column
     };
-    return <Table rowSelection={rowSelection} columns={columns} dataSource={data} />;
-  }
+
+    onSelectChange = selectedRowKeys => {
+        console.log('selectedRowKeys changed: ', selectedRowKeys);
+        this.setState({
+            selectedRowKeys
+        });
+    };
+
+    render() {
+        const {
+            selectedRowKeys
+        } = this.state;
+        const rowSelection = {
+            selectedRowKeys,
+            onChange: this.onSelectChange,
+            hideDefaultSelections: true,
+            selections: [
+                {
+                    key: 'odd',
+                    text: '选择奇数',
+                    onSelect: changableRowKeys => {
+                        let newSelectedRowKeys = [];
+                        newSelectedRowKeys = changableRowKeys.filter((key, index) => {
+                            if (index % 2 !== 0) {
+                                return false;
+                            }
+                            return true;
+                        });
+                        this.setState({
+                            selectedRowKeys: newSelectedRowKeys
+                        });
+                    },
+                },
+                {
+                    key: 'even',
+                    text: '选择偶数',
+                    onSelect: changableRowKeys => {
+                        let newSelectedRowKeys = [];
+                        newSelectedRowKeys = changableRowKeys.filter((key, index) => {
+                            if (index % 2 !== 0) {
+                                return true;
+                            }
+                            return false;
+                        });
+                        this.setState({
+                            selectedRowKeys: newSelectedRowKeys
+                        });
+                    },
+                },
+            ],
+        };
+        return <Table
+        rowSelection = {
+            rowSelection
+        }
+        columns = {
+            columns
+        }
+        dataSource = {
+            data
+        }
+        />;
+    }
 }
 
 export default HotBooks
